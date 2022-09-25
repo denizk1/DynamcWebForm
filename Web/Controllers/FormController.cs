1b2e5493ede1e41d11d08cf9b8a8324f7a7d58ff
Deniz Kaparlar <kaparlardeniz@gmail.com>
2022-09-25 19:10:43 +0300

    Katmanlar oluşturuldu. Menu ve itemları eklendi

---
+++ Web/Controllers/FormController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FormList()
+        {
+            return View();
+        }
+        public IActionResult CreateForm()
+        {
+            return View();
+        }
+    }
+}
